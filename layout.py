e758cc6f5b1c377cc1c766845d6b93ae37a3393a
Chan647 <cksgml8760@gmail.com>
2025-09-29 16:25:14 +0900

    PyQt layout practice

---
+++ layout.py
@@ -0,0 +1,28 @@
+import sys
+from PyQt5.QtWidgets import *
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        window = QWidget()
+        layout = QGridLayout()#QHlayout, QVlayout
+        layout.addWidget(QPushButton("(0,0)"),0,0)
+        layout.addWidget(QPushButton("(0,1)"),0,1)
+        layout.addWidget(QPushButton("(1,0)"),1,0)
+        layout.addWidget(QPushButton("(1,1)"),1,1)
+
+        '''
+        layout.addWidget(QPushButton("Button 1"))
+        layout.addWidget(QPushButton("Button 2"))
+        layout.addWidget(QPushButton("Button 3"))
+        '''
+        window.setLayout(layout)
+        self.setCentralWidget(window)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = Window()
+    myWindow.show()
+    app.exec_()
---
+++ signal_slot.py
@@ -0,0 +1,31 @@
+import sys
+import random
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+form_class = uic.loadUiType("line.ui")[0]
+
+class Window(QMainWindow, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        
+        # self.pushButton.setText("Push")
+        # self.pushButton.clicked.connect(self.random_color)
+        self.lineEdit.setPlaceholderText("여기에 입력하세요")
+        self.lineEdit.textChanged.connect(self.random_color)
+        # self.lineEdit.returnPressed.connect(self.random_color)
+
+    def random_color(self):
+       r = random.randint(0, 255)
+       g = random.randint(0, 255)
+       b = random.randint(0, 255)
+
+       self.lineEdit.setStyleSheet(f"background-color: rgb({r},{g},{b})")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = Window()
+    myWindow.show()
+    app.exec_()
